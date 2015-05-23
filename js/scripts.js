55ae3c3b8070cd0bc187d9aaa1821d5ec02c8cec
Chris Woodhouse <itstime2gohunting@gmail.com>
2015-05-22 21:29:24 -0400

    Added jQuery script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 function getDate() {
 				document.getElementById("date").innerHTML = Date();
-			}	
+			}
+
+//SAME CODE AS BELOW WITH ANONYMOUS FUNCTION
+//jQuery(document).ready(function () {
+//	jQuery(".h1Class").hide().show("slow");
+//});
+
+function myCode() {
+	$(".h1Class"). hide().show("slow");
+};
+
+$(document).ready(myCode);
